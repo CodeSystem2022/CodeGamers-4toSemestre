d633afb0fd9eb1517ac0de23318bc286304d885a
NathalieDS <112034718+NathalieDS@users.noreply.github.com>
2023-11-09 01:14:50 +0100

    Add files via upload

---
+++ Python/tipo_str.py
@@ -0,0 +1,11 @@
+import math
+# Profundizando en el tipo String
+# Concatenacion automarica en Python
+
+variable = ' Adios'
+mensaje = 'Hola ''Alumnos' + variable
+mensaje += ', Terminamos'
+print(mensaje)
+
+# Usamos la clase help para ayuda o ducumentacion (built-in)
+help(math.isnan)
